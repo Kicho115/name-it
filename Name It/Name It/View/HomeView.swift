59def9e4ccbac5a5927d9dba8016a7684414f0f3
Kicho115 <oscar.leonardo.ang@gmail.com>
2025-04-09 19:16:07 -0700

    Refactor: Move ToogleRow into its own component

--- Name It/Name It/View/HomeView.swift
+++ Name It/Name It/View/HomeView.swift
@@ -92,66 +92,6 @@ struct HomeView: View {
     }
 }
 
-// ToggleRow component
-struct ToggleRow: View {
-    let icon: String
-    let title: String
-    @Binding var isOn: Bool
-
-    var body: some View {
-        HStack {
-            HStack(spacing: 12) {
-                Image(systemName: icon)
-                .foregroundColor(.white)
-                .frame(width: 24, height: 24)
-
-                Text(title)
-                    .font(.system(size: 16))
-                    .foregroundColor(.white)
-            }
-
-            Spacer()
-
-            Toggle("", isOn: $isOn)
-                .labelsHidden()
-                .toggleStyle(CustomToggleStyle())
-        }
-        .padding(16)
-        .background(Color.white.opacity(0.06))
-        .cornerRadius(12)
-    }
-}
-
-struct CustomToggleStyle: ToggleStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        HStack {
-            configuration.label
-
-            ZStack(alignment: configuration.isOn ? .trailing : .leading) {
-                RoundedRectangle(cornerRadius: .infinity)
-                    .frame(width: 48, height: 24)
-                    .foregroundColor(
-                        configuration.isOn ?
-                        Color(red: 67/255, green: 24/255, blue: 209/255) :
-                        Color.white.opacity(0.227)
-                    )
-
-                if configuration.isOn {
-                    Circle()
-                        .frame(width: 20, height: 20)
-                        .foregroundColor(.white)
-                        .padding(2)
-                }
-            }
-            .onTapGesture {
-                withAnimation(.spring()) {
-                    configuration.isOn.toggle()
-                }
-            }
-        }
-    }
-}
-
 #Preview {
     HomeView()
 }
